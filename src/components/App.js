eadac2cb85cc21956d8d5c8fe2d42ebb31f4986f
Nikhil <ns345360@gmail.com>
2022-12-05 14:22:41 +0530

    set up the customFetch function in api folder and designed the home page component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
+import { getPosts } from '../api/index';
+import { Home } from '../pages/index';
 
 function App() {
+  useEffect(() => {
+    //making a function and then calling it, because the callback function passed to used effect has to be synchronous(so can't be async one)
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log('response', response);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Home />
     </div>
   );
 }
