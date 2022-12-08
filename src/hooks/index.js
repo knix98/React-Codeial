80494f3e900904880f04b6c66498fcd797966f2b
Nikhil <ns345360@gmail.com>
2022-12-08 13:43:41 +0530

    implemented the sign-up functionality for the register page

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,7 +2,7 @@ import { useContext, useState, useEffect } from 'react';
 import jwt from 'jwt-decode';
 
 import { AuthContext } from '../providers/AuthProvider';
-import { login as userLogin } from '../api/index';
+import { login as userLogin, register } from '../api/index';
 import {
   setItemInLocalStorage,
   LOCALSTORAGE_TOKEN_KEY,
@@ -56,6 +56,21 @@ export const useProvideAuth = () => {
     }
   };
 
+  const signup = async (name, email, password, confirmPassword) => {
+    const response = await register(name, email, password, confirmPassword);
+
+    if (response.success) {
+      return {
+        success: true,
+      };
+    } else {
+      return {
+        success: false,
+        message: response.message,
+      };
+    }
+  };
+
   const logout = () => {
     //remove JWT from local storage when user logs out
     removeItemFromLocalStorage(LOCALSTORAGE_TOKEN_KEY);
@@ -68,5 +83,6 @@ export const useProvideAuth = () => {
     login,
     logout,
     loading,
+    signup,
   };
 };
